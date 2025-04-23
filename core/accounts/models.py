4b838082b3441681be02ec8f878eacdfb28b9959
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-23 14:50:28 +0330

    set setting for know my User model
    user manager config

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verified = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=20)
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
     created_at = models.DateTimeField(auto_now_add=True)
--- core/core/settings.py
+++ core/core/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
+    # 'blog',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +138,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User manager config
+AUTH_USER_MODEL = 'accounts.User'
