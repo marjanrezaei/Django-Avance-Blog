df0e89fa3d442bd384e8183ee13f4041efd1ee79
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-24 18:18:07 +0330

    create Profile class
    create 'class Profile' in models.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 
 # Register your models here.
 
@@ -44,3 +44,4 @@ class CustomUserAdmin(UserAdmin):
     )
     
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -54,3 +54,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.email
+    
+    
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        self.user.email
