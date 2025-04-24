b413d5dbd7095d3eecea1160f9b2de78e9ba0f5a
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-24 19:10:36 +0330

    connect User and Profile model
    create connect between user and Profile
    after save user model create user in profile
    this things do with signals

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class UserManager(BaseUserManager):
     """
@@ -66,4 +67,10 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        self.user.email
+       return  self.user.email
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
