b615bf88f3f33ce19eadd7367af400f1b02a5790
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-25 13:07:13 +0330

    TemplateView Test

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import indexView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', indexView, name='fbv-test'),
+    path('fbv-index', indexView, name='fbv-test'),
+    path('cbv-index', TemplateView.as_view(template_name = "index.html", extra_context = {"name":"marjan"})),
+    
 ]
