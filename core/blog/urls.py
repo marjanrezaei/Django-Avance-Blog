d7e5d005bcb52a78447155a63622cefc12d9cf2a
marjanrezaei <rezaei.marjann@gmail.com>
2025-04-25 15:43:39 +0330

    redirect Pattern

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,9 +3,11 @@ from . import views
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
+app_name = "blog"
+
 urlpatterns = [
     path('fbv-index', views.indexView, name='fbv-test'),
     # path('cbv-index', TemplateView.as_view(template_name = "index.html", extra_context = {"name":"marjan"})),
     path('cbv-index',views.IndexView.as_view(), name = 'cbv-index'),
-    path('go-to-google', RedirectView.as_view(url="https://google.com/"), name='redirect-to-google')
+    path('go-to-index', RedirectView.as_view(pattern_name="blog:cbv-index"), name='redirect-to-index')
 ]
