3cd878dab156184cde23a7037cda3979c0254d2e
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-04 13:06:22 +0330

    handle 404 with get-object-or-404

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,8 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
-from rest_framework import status
-
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
@@ -12,9 +11,6 @@ def postList(request):
 
 @api_view()
 def postDetail(request, id):
-    try:
-        post = Post.objects.get(pk=id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    except Post.DoesNotExist:
-        return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+    post = get_object_or_404(Post, pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
