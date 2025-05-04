a8a2f999b36a9243e9fd34c5b958b591c99d2c95
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-04 13:04:36 +0330

    handle 404 with try
    handle posts that didn't exist with try except and show with status

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
 
 
 @api_view()
@@ -11,6 +12,9 @@ def postList(request):
 
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    try:
+        post = Post.objects.get(pk=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
