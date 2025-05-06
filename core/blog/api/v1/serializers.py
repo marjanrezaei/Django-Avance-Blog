a9f1f878ff77cfe58775237a3c5d4c864d688e9b
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-06 16:16:38 +0330

    Get full path with serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from ...models import Post, Category
+from django.urls import reverse
 
 
 # class PostSerializer(serializers.Serializer):
@@ -10,12 +11,19 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
-    
+    absolute_url = serializers.SerializerMethodField()
     
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'snippet', 'status','relative_url', 'created_at', 'published_at'] 
-     
+        fields = ['id', 'author', 'title', 'content', 'snippet', 'status','relative_url', 'absolute_url', 'created_at', 'published_at'] 
+    
+    def get_absolute_url(self, obj):
+        request = self.context.get('request')
+        if request:
+            return request.build_absolute_uri(reverse('blog:api-v1:post-detail', kwargs={'pk': obj.pk}))
+        return reverse('blog:api-v1:post-detail', kwargs={'pk': obj.pk})
+
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
