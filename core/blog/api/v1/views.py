28ce578d19fc095b8ab44b209679381bedcd9451
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-07 19:35:29 +0330

    Pagination for posts

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -11,6 +11,7 @@ from rest_framework.decorators import action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from .paginations import DefaultPagination
 
 '''
 from rest_framework.decorators import api_view, permission_classes
@@ -117,6 +118,7 @@ class PostModelViewSet(viewsets.ModelViewSet):
     filterset_fields = ['category', 'author','status']
     search_fields = ['title', 'content']
     ordering_fields = ['published_at']
+    pagination_class = DefaultPagination
    
    
 class CategoryModelViewSet(viewsets.ModelViewSet):
