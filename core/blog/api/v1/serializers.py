b5a51bf0570bc84e499c6cdab139ccb63616d414
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-07 13:39:05 +0330

    show Category name with SlugRelatedField

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,14 +8,22 @@ from django.urls import reverse
 #     title = serializers.CharField(max_length=255)
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+        
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url = serializers.SerializerMethodField()
+    # category = serializers.SlugRelatedField(many=False, slug_field = 'name', queryset= Category.objects.all())
+    category = CategorySerializer()
+    
     
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'snippet', 'status','relative_url', 'absolute_url', 'created_at', 'published_at'] 
+        fields = ['id', 'author', 'title', 'content', 'snippet', 'status', 'category','relative_url', 'absolute_url', 'created_at', 'published_at'] 
     
     def get_absolute_url(self, obj):
         request = self.context.get('request')
@@ -23,9 +31,3 @@ class PostSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(reverse('blog:api-v1:post-detail', kwargs={'pk': obj.pk}))
         return reverse('blog:api-v1:post-detail', kwargs={'pk': obj.pk})
 
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
