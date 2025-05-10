a0f4a46720aa0fc534b24f877b8ec797cd5bebc4
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-10 19:07:14 +0330

    Registration API

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path, include
+from .import views
 
-urlpatterns = []
+app_name = 'api-v1'
+
+urlpatterns = [
+    # registration
+    path('registration/', views.RegistrationApiView.as_view(), name='registration'),   
+    
+    # change password 
+    # reset password
+    # login token
+    # login jwt
+    
+]
