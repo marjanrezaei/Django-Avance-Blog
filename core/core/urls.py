54dc46df367367050f0f4367c3f0077498a129ea
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-10 18:02:35 +0330

    API accounts - Basic settings

--- core/core/urls.py
+++ core/core/urls.py
@@ -39,7 +39,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('blog/', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
