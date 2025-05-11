b0bee0cecc18220114e0aa6a147b298f7a950f57
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-11 14:44:21 +0330

    Validation for email and password

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 from ...models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(max_length = 255, write_only=True)
+
+
     class Meta:
         model = User
-        fields = ['email', 'password']
-        
+        fields = ['email', 'password', 'password1']
     
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('password1'):
+            raise serializers.ValidationError("Passwords do not match")
+        
+        try:
+            validate_password(attrs.get('password'))
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+        
+        return super().validate(attrs)
+        
+    def create(self, validated_data):
+        validated_data.pop('password1')
+        return User.objects.create_user(**validated_data) # create_user is a method in the User model that creates a user with the given email and password 
