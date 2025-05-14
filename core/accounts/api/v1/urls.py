a0181ef5ad5adaa68ed445c89fbbad3bc5457d8e
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-14 20:40:07 +0330

    change url

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -7,11 +7,12 @@ app_name = 'api-v1'
 urlpatterns = [
     # registration
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),   
-    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
-    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+   
     # change password 
     # reset password
     # login token
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
     # login jwt
     
 ]
