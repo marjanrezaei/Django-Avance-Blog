3df4a90bcce4a000d33f04284ef2bf21978dfcad
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-16 15:28:05 +0330

    Separate User model with Profile model
    this trick useful for serializers and views

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 # Create your models here.
 class UserManager(BaseUserManager):
     """
@@ -56,21 +56,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
     
-    
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    image = models.ImageField(blank=True, null=True)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-       return  self.user.email
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
