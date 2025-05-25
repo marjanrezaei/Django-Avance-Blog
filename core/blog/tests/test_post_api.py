86bd920f65016accf7fafcead71239c04f32aefb
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-25 17:49:19 +0330

    real test in API

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,7 +1,17 @@
+from rest_framework.test import APIClient
+from django.urls import reverse
 import pytest
 
 
-class TestPostAPI:
-    def test_get_post_request_200_status(self):
-        assert 1 == 1
-        
+@pytest.mark.django_db
+class TestPostApi:
+
+    def test_get_post_response_200(self, django_user_model):
+        user = django_user_model.objects.create_user(email="test@example.com", password="testpass")
+        client = APIClient()
+        client.force_authenticate(user=user)
+        url = reverse("blog:api-v1:post-list")
+        response = client.get(url)
+        assert response.status_code == 200
+     
+
