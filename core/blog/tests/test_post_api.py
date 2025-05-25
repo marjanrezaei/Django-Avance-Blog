4d7a7dc58d687ddf0ba983028cbbaaad00162e9d
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-25 18:45:02 +0330

    Request for create post if user don't logged in

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,17 +1,25 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+from datetime import datetime
 
 
 @pytest.mark.django_db
 class TestPostApi:
-
+    client = APIClient()
+    
     def test_get_post_response_200(self, django_user_model):
-        user = django_user_model.objects.create_user(email="test@example.com", password="testpass")
-        client = APIClient()
-        client.force_authenticate(user=user)
         url = reverse("blog:api-v1:post-list")
-        response = client.get(url)
-        assert response.status_code == 200
+        response = self.client.get(url)
+        assert response.status_code == 401
      
-
+    def test_create_post_response_401_status(self):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "Test Post",
+            "content": "This is a test post.",
+            "status": True,
+            "published_at": datetime.now(),
+        }
+        response = self.client.post(url, data)
+        assert response.status_code == 401
