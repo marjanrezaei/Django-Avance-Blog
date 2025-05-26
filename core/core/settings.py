72873839cc4b5691131e2f91ca361f0691f4988a
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-26 13:11:39 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,7 +27,7 @@ DATABASES = {
         "PORT": config("DB_PORT", default=5432, cast=int),
     }
 }
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'..
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
