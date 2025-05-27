d7b792b9692dd6167f3d39e73b7037e1246e248d
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-27 20:31:52 +0330

    Authenticate User before Locust request

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -2,6 +2,16 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     
+    def on_start(self):
+        """ on_start is called when a Locust user starts before any task is scheduled """
+        response = self.client.post("/accounts/api/v2/jwt/create/", {
+            "email": "marjan@gmail.com",
+            "password": "123"
+        }).json()
+        self.client.headers.update({
+            "Authorization": f"Bearer {response['access']}"
+        })
+
     @task
     def post_list(self):
         self.client.get("/blog/api/v1/post/")
