4fa15dac4ee8f004c0997095c55e2251f6db5188
marjanrezaei <rezaei.marjann@gmail.com>
2025-05-28 17:35:41 +0330

    Install django-celery-beat in backend & worker
    python manage.py migrate
    
    docker-compose exec backend celery -A core beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler

--- core/core/celery.py
+++ core/core/celery.py
@@ -17,6 +17,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender: Celery, **kwargs):
-    sender.add_periodic_task(10.0, sendEmail.s(), name='send email every 10 seconds')
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     sender.add_periodic_task(10.0, sendEmail.s(), name='send email every 10 seconds')
--- core/core/settings.py
+++ core/core/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     "mail_templated",
     "djoser",
     "corsheaders",
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
