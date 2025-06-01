a06ea4e79900d1084dbc253da3bc78d6b8d51291
marjanrezaei <rezaei.marjann@gmail.com>
2025-06-01 15:41:13 +0330

    show weather every 20 min

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("api/v1/", include("accounts.api.v1.urls")),
     path("send-email/", views.send_email, name="send_email"),
     path("test/", views.test, name="test"),
+    path('weather/', views.weather, name='weather-api'),
     # path('api/v2/', include('djoser.urls')),
     # path('api/v2/', include('djoser.urls.jwt')),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -3,6 +3,8 @@ import requests
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from .tasks import sendEmail 
+import os
+
 
 def send_email(request):
     """
@@ -13,7 +15,25 @@ def send_email(request):
     # Return a simple response
     return HttpResponse("Email sent successfully!")
 
+
 @cache_page(60 * 15)  # Cache this view for 15 minutes
 def test(request):
     response = requests.get("https://e36589a2-9f85-4ac0-8dcf-f7b98135d9e9.mock.pstmn.io/test/delay/5")
     return JsonResponse(response.json())
+
+
+@cache_page(60 * 20)  # Cache for 20 minutes
+def weather(request):
+    api_key = "8f160f8d25de806ef110dbabe5cb8f50"
+    if not api_key:
+        return JsonResponse({"error": "API key not set"}, status=500)
+    city = "London"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=8f160f8d25de806ef110dbabe5cb8f50"
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return JsonResponse(data)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
